e49fc98e055823637ffa509ad0c1d70507b0daaa
ding <1621029739@qq.com>
2024-06-24 18:00:11 +0800

    动态添加空间结合前端

---
+++ src/components/control/ControlCustom.js
@@ -0,0 +1,39 @@
+class ControlCustom {
+    constructor(map) {
+        this.map = map;
+        this.controlList = [];
+        this._getObj();
+    }
+    _getObj() {
+        let obj = { controlList: [] };
+        this.map.setOptions(obj);
+        const mapJSON = this.map.toJSON();
+        if (mapJSON?.options?.controlList) {
+            this.controlList = mapJSON.options.controlList;
+        }
+    }
+    add(control) {
+        if (!this._has(control.type)) {
+            this.controlList.push(control);
+        }
+    }
+    remove(type) {
+        this.controlList = this.controlList.filter(ctrl => ctrl.type !== type);
+        this._updateMapSettings();
+    }
+    update(type, options) {
+        const index = this.controlList.findIndex(ctrl => ctrl.type === type);
+        if (index !== -1) {
+            this.controlList[index] = { ...this.controlList[index], ...options };
+            this._updateMapSettings();
+        }
+    }
+    _has(type) {
+        return this.controlList.some(ctrl => ctrl.type === type);
+    }
+    _updateMapSettings() {
+        this.map.setOptions({ controlList: this.controlList });
+    }
+}
+
+export default ControlCustom;
--- src/components/control/control_custom.vue
+++ src/components/control/control_custom.vue
@@ -1,63 +1,13 @@
 <template>
   <div>
     <div id="map" class="container"></div>
-    <div class="control"></div>
-    <div id="json"></div>
-    <div class="locationSearch">
-      <div class="inputBox">
-        <a-input
-          placeholder="请输入地点名称"
-          :value="inputValue"
-          @change="handlechangeInputvalue"
-        />
-        <span
-          style="width: 19px; height: 17px"
-          class="eliminateinput"
-          v-show="inputValue != ''"
-          @click="handleClearinput"
-          >X</span
-        >
-        <a-button class="searchBtn" type="primary" @click="handleSeach"
-          ><img src="images/icon/fdj.png" style="width: 20px; height: 20px"
-        /></a-button>
-      </div>
-      <div class="conbox" v-show="contentshow">
-        <ul>
-          <li v-for="item in contentList" :key="item.id" @click="handleloadDetail(item)">
-            <a-tooltip
-              style="
-                width: 150px;
-                white-space: nowrap;
-                overflow: hidden;
-                text-overflow: ellipsis;
-              "
-            >
-              <template slot="title">
-                <p>{{ item.name }}</p>
-              </template>
-              {{ item.name }}
-            </a-tooltip>
-            <a-tooltip
-              style="
-                width: 150px;
-                white-space: nowrap;
-                overflow: hidden;
-                text-overflow: ellipsis;
-              "
-            >
-              <template slot="title">
-                <p>{{ item.address }}</p>
-              </template>
-              {{ item.address }}
-            </a-tooltip>
-          </li>
-        </ul>
-        <div class="zwsj" v-show="contentList.length == 0">
-          <p><img src="images/icon/zwsj.png" /></p>
-          <span>暂无数据</span>
-        </div>
-      </div>
+    <div class="control">
+      <button @click="addScale">增缩放</button>
+      <button @click="addLegend">增图例</button>
+      <button @click="remove('legend')">删</button>
+      <button @click="update('legend', { bgColor: '#ding' })">改</button>
     </div>
+    <div id="json"></div>
   </div>
 </template>
 
@@ -65,18 +15,13 @@
 import "maptalks/dist/maptalks.css";
 import { Map, TileLayer, Marker, control } from "maptalks";
 import { GroupGLLayer, PointLayer } from "@maptalks/gl-layers";
-import axios from "axios";
-
+import ControlCustom from "./ControlCustom";
 export default {
   components: {},
-
   data() {
     return {
       map: null,
-      //搜索
-      inputValue: "",
-      contentshow: false,
-      contentList: [],
+      cl: null,
     };
   },
 
@@ -99,68 +44,52 @@ export default {
           '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>',
       }),
       layers: [],
-      attribution: true,
-      zoomControl: {
-        position: { bottom: "230", right: "10" },
-        slider: true,
-        zoomLevel: false,
-      }, // add zoom control
-      //scaleControl: true, // add scale control
-      //overviewControl: true, // add overview control
+      attribution: false,
+      // zoomControl: {
+      //   position: { bottom: "230", right: "10" },
+      //   slider: true,
+      //   zoomLevel: false,
+      // },
     });
 
+    this.cl = new ControlCustom(this.map);
+
     /**
-     * 可以这么存储样式信息
+     * 添加到map上
      */
-    // let options2 = {
-    //   panel: {
-    //     position: { bottom: "230", right: "10" },
-    //     draggable: true, //拖拽
-    //     custom: false, //定制
-    //     closeButton: true,
-    //   },
+    // let obj = {
+    //   controlList: [
+    //     {
+    //       type: "scale",
+    //       display: true,
+    //       position: [116.37405, 39.93185],
+    //       bgColor: "#fff",
+    //       bgImg: "images/icon/icon_Red.png",
+    //     },
+    //     {
+    //       type: "legend",
+    //       display: true,
+    //       position: [116.37405, 39.93185],
+    //       bgColor: "#fff",
+    //       bgImg: "images/icon/icon_Red.png",
+    //     },
+    //     {
+    //       type: "search",
+    //       display: true,
+    //       position: [116.37405, 39.93185],
+    //       bgColor: "#fff",
+    //       bgImg: "images/icon/icon_Red.png",
+    //     },
+    //     {
+    //       type: "tools",
+    //       display: true,
+    //       position: [116.37405, 39.93185],
+    //       bgColor: "#fff",
+    //       bgImg: "images/icon/icon_Red.png",
+    //     },
+    //   ],
     // };
-    // this.map.setOptions(options2);
-    /**
-     * 图层控制
-     */
-    // let layerSwitcher = new control.LayerSwitcher({
-    //   position: "top-right",
-    //   baseTitle: "底图图层集合",
-    //   overlayTitle: "其他图层集合",
-    //   excludeLayers: [], //排除图层
-    //   containerClass: "maptalks-layer-switcher", //css样式
-    // });
-    // this.map.addControl(layerSwitcher);
-
-    /**
-     * 添加图层
-     */
-    this.groupLayer = new GroupGLLayer("GL组", [], {
-      sceneConfig: {
-        environment: {
-          enable: true,
-          mode: 1,
-          level: 0,
-          brightness: 0,
-        },
-      },
-    }).addTo(this.map);
-    this.add_GroupGL_PointLayer();
-
-    /**
-     * 动态添加组件
-     */
-    let panel = new control.Panel({
-      position: { bottom: "220", right: "0" },
-      draggable: true, //拖拽
-      custom: false, //定制
-      closeButton: true,
-      content: "<div>自定义面板</div>", // 面板的内容，可以是 HTML 代码
-    });
-    this.map.addControl(panel);
-    this.add_Scale();
-
+    // this.map.setOptions(obj);
     /**
      * toJSON
      */
@@ -170,315 +99,52 @@ export default {
   },
 
   methods: {
-    add_GroupGL_PointLayer() {
-      const pointLayer = new PointLayer("点");
-      const marker = new Marker([116.39225, 39.90605], {
-        cursor: "pointer",
-        symbol: {
-          markerFile: "images/icon/icon_Red.png",
-          markerOpacity: 1,
-          markerWidth: 28,
-          markerHeight: 40,
-          textName: "GroupGL矢量点",
-        },
-      }).addTo(pointLayer);
-      this.groupLayer.addLayer(pointLayer);
-    },
-
     toJson() {
+      document.getElementById("json").innerHTML = "";
       const mapJSON = this.map.toJSON();
       document.getElementById("json").innerHTML = JSON.stringify(mapJSON);
     },
-
-    /**
-     * 比例尺控件
-     */
-    add_Scale() {
-      let scale = new control.Scale({
-        position: "top-right",
-        containerClass: "scaleCss",
-      });
-      this.map.addControl(scale);
-    },
-
-    /**
-     * 搜索控件
-     */
-    //平台版本
-    // async handleSeach() {
-    //   this.contentList = [];
-    //   if (this.inputValue === "") {
-    //     this.$message.error("请输入关键字！");
-    //     return;
-    //   }
-    //   //使用后台配置天地图服务接口查询poi
-    //   var response = await searchUrlList();
-    //   if (response.success) {
-    //     if (response.result) {
-    //       var poiUrl = response.result.poiUrl;
-    //       var supplier = poiUrl.supplier;
-    //       var url = poiUrl.url;
-
-    //       if (supplier === "tdt") {
-    //         try {
-    //           url = url.replace("keyValue", this.inputValue);
-    //           const response = await fetch(url);
-    //           const result = await response.json();
-    //           if (
-    //             result.status &&
-    //             result.status.infocode &&
-    //             result.status.infocode == 1000
-    //           ) {
-    //             for (var i = 0; i < result.pois.length; i++) {
-    //               var poi = result.pois[i];
-    //               var lonlat = poi.lonlat;
-    //               var lon = parseFloat(lonlat.split(",")[0]);
-    //               var lat = parseFloat(lonlat.split(",")[1]);
-    //               var geometry = { type: "Point", coordinates: [lon, lat] };
-    //               var position = {
-    //                 name: poi.name,
-    //                 address: poi.address,
-    //                 geometry: geometry,
-    //               };
-    //               this.contentList.push(position);
-    //             }
-    //           } else {
-    //             // this.$message.error("天地图在线查询失败！");
-    //           }
-    //         } catch (e) {
-    //           console.log(e);
-    //           this.$message.error("天地图在线查询失败！");
-    //         }
-    //       }
-    //     }
-    //   } else {
-    //     this.$message.error("获取服务地址失败！");
-    //     return;
-    //   }
-    //   this.contentshow = true;
-    // },
-
-    //天地图版本
-    handleSeach() {
-      this.contentList = [];
-      if (this.inputValue === "") {
-        this.$message.error("请输入关键字！");
-        return;
-      }
-      //天地图视野内搜索服务
-      const url = "http://api.tianditu.gov.cn/v2/search";
-      const extent = this.map.getExtent();
-      const mapBound = `${extent.xmin},${extent.ymin},${extent.xmax},${extent.ymax}`;
-      const params = {
-        postStr: JSON.stringify({
-          keyWord: this.inputValue,
-          level: parseInt(this.map.getZoom()),
-          mapBound: mapBound,
-          queryType: 2,
-          start: 0,
-          count: 10,
-        }),
-        type: "query",
-        tk: "21c1e34286caecc25fd94be94bfbe119",
+    addScale() {
+      let scale = {
+        type: "scale",
+        display: true,
+        alias:"缩放控件",
+        position: [116.37405, 39.93185],
+        bgColor: "#fff",
+        bgImg: "images/icon/icon_Red.png",
       };
-      axios({
-        method: "GET",
-        url: url,
-        params: params,
-      })
-        .then((res) => {
-          if (res?.status == 200) {
-            for (var i = 0; i < res.data.pois.length; i++) {
-              var poi = res.data.pois[i];
-              var lonlat = poi.lonlat;
-              var lon = parseFloat(lonlat.split(",")[0]);
-              var lat = parseFloat(lonlat.split(",")[1]);
-              var geometry = { type: "Point", coordinates: [lon, lat] };
-              var position = {
-                name: poi.name,
-                address: poi.address,
-                geometry: geometry,
-              };
-              this.contentList.push(position);
-            }
-          }
-        })
-        .catch((error) => {
-          this.$message.error("天地图在线查询失败！");
-        });
-      this.contentshow = true;
+      this.cl.add(scale);
+      this.toJson();
     },
-    handlechangeInputvalue(e) {
-      this.inputValue = e.target.value;
+    addLegend() {
+      let legend = {
+        type: "legend",
+        display: true,
+        alias:"图例控件",
+        position: [116.37405, 39.93185],
+        bgColor: "#fff",
+        bgImg: "images/icon/icon_Red.png",
+      };
+      this.cl.add(legend);
+      this.toJson();
     },
-    handleClearinput() {
-      this.inputValue = "";
-      this.contentshow = false;
+    remove(type) {
+      this.cl.remove(type);
+      this.toJson();
     },
-    handleloadDetail(item) {
-      if (!item.geometry.coordinates) return;
-      let seachPointLayer = this.map.getLayer("seachPoint");
-      if (seachPointLayer) {
-        this.map.removeLayer(seachPointLayer);
-      }
-      const pointLayer = new PointLayer("seachPoint");
-      const marker = new Marker(item.geometry.coordinates, {
-        cursor: "pointer",
-        symbol: {
-          markerFile: "images/icon/icon_Red.png",
-          markerOpacity: 1,
-          markerWidth: 28,
-          markerHeight: 40,
-        },
-      }).addTo(pointLayer);
-      this.map.addLayer(pointLayer);
-      this.map.flyTo(
-        {
-          zoom: 18,
-          center: item.geometry.coordinates,
-          pitch: this.map.getPitch(),
-          bearing: this.map.getBearing(),
-        },
-        {
-          duration: 3000,
-          easing: "out",
-        }
-      );
+    update(type, options) {
+      this.cl.update(type, options);
+      this.toJson();
     },
-
-    /**
-     * 图例控件
-     */
-    add_legend() {},
   },
 };
 </script>
 <style lang="less" scope>
-.locationSearch {
-  position: absolute;
-  top: 90px;
-  right: 20px;
-  z-index: 9;
-  display: flex;
-  .inputBox {
-    width: 316px;
-    height: 40px;
-    display: flex;
-    background: rgba(9, 28, 55, 0.7);
-    box-shadow: inset 0px 1px 12px 0px #4389ff;
-    border-radius: 4px;
-    input {
-      width: 269px;
-      height: 40px;
-      border: none;
-    }
-    .ant-input {
-      background-color: transparent;
-      color: #ffffff;
-    }
-    .eliminateinput {
-      position: absolute;
-      top: 10px;
-      right: 40px;
-      cursor: pointer;
-      color: #ffffff;
-    }
-    .searchBtn {
-      height: 40px;
-      width: 40px;
-      position: absolute;
-      right: 0;
-      padding: 0;
-      border-radius: 0 4px 4px 0;
-      background-color: transparent;
-    }
-  }
-  .conbox {
-    width: 316px;
-    max-height: 374px;
-    background: rgba(9, 28, 55, 0.7);
-    box-shadow: inset 0px 1px 12px 0px #4389ff;
-    position: absolute;
-    right: 0;
-    top: 40px;
-    overflow-y: auto;
-    .zwsj {
-      margin-top: 50px;
-      text-align: center;
-      padding-bottom: 50px;
-      span {
-        display: block;
-        margin-top: 15px;
-        color: #ffffff;
-      }
-    }
-    ul {
-      padding-left: 0px;
-      li {
-        display: flex;
-        padding: 5px 10px;
-        cursor: pointer;
-        p {
-          font-size: 14px;
-          font-weight: 400;
-          color: #ffffff;
-          width: 210px;
-          white-space: nowrap;
-          overflow: hidden;
-          text-overflow: ellipsis;
-        }
-        span {
-          margin-left: 10px;
-          font-size: 12px;
-          font-weight: 400;
-          color: #ffffff;
-          text-align: left;
-        }
-      }
-    }
-  }
-  .conbox::-webkit-scrollbar {
-    /*滚动条整体样式*/
-    width: 4px; /*高宽分别对应横竖滚动条的尺寸*/
-    height: 4px;
-    border-radius: 6px;
-  }
-  .conbox::-webkit-scrollbar-thumb {
-    /*滚动条里面小方块*/
-    border-radius: 4px;
-    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
-    background: rgba(23, 133, 255, 1);
-  }
-  .conbox::-webkit-scrollbar-track {
-    /*滚动条里面轨道*/
-    height: 90%;
-    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
-    border-radius: 4px;
-    background: rgba(235, 235, 235, 1);
-  }
-}
-.scaleCss {
-  border: 2px solid #000000;
-  border-top: none;
-  line-height: 1.1;
-  padding: 0px;
-  color: #000000;
-  font-size: 11px;
-  text-align: center;
-  white-space: nowrap;
-  overflow: hidden;
-  -moz-box-sizing: content-box;
-  box-sizing: content-box;
-  background-image: url("../../../public/images/icon/icon_Red.png"); /* 设置图片路径 */
-  background-size: cover; /* 背景图片覆盖整个元素 */
-  background-repeat: no-repeat; /* 背景图片不重复 */
-  background-position: center; /* 背景图片居中 */
-}
 .control {
   position: absolute;
   z-index: 999;
   left: 10px;
-  top: 10px;
+  top: 190px;
 }
 #json {
   position: fixed;
