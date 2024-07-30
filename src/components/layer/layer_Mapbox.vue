428f51b57c4104d161a72d6e015ec307db09242e
ding <1621029739@qq.com>
2024-07-30 10:54:55 +0800

    添加layer_Mapbox

--- src/components/layer/layer_Mapbox.vue
+++ src/components/layer/layer_Mapbox.vue
@@ -24,7 +24,7 @@ export default {
 
   mounted() {
     this.map = new maptalks.Map("map", {
-      center: [116.414390,39.916710],
+      center: [116.41439, 39.91671],
       zoom: 15,
       pitch: 60,
       bearing: -25,
@@ -50,20 +50,36 @@ export default {
       sceneConfig,
     });
     this.groupLayer.addTo(this.map);
-    this.addMapBoxLayer()
+    this.addMapBoxLayer();
+    //this.addGeoServerLayer()
   },
 
   methods: {
     /**
-     * 矢量切片服务-tileserver
-     * 样式-MapTalks Designer制作（与mapbox不是一套）
+     * ①矢量切片服务-tileserver
+     * ②样式-MapTalks Designer制作（与mapbox不是一套）
+     * ③MapTalks Designer设计的时候要添加style地址，因为里面包含pbf地址了-http://192.168.201.166:8080/style/dongcheng/default_blue/style.json
      */
     addMapBoxLayer() {
-      const vt = new VectorTileLayer("vt", {
-        urlTemplate:`http://192.168.201.166:8081/data/dongcheng/{z}/{x}/{y}.pbf`,
+      const vt = new VectorTileLayer("mapbox_vt", {
+        urlTemplate: `http://192.168.201.166:8081/data/dongcheng/{z}/{x}/{y}.pbf`,
         style: "data/json/mapbox-light/style_mapbox_dongcheng.json",
       });
-      this.groupLayer.addLayer(vt)
+      this.groupLayer.addLayer(vt);
+    },
+    /**
+     *  ①矢量切片服务-geoserver
+     *  ②options上设置tms: true
+     *  ③MapTalks Designer设计的时候要添加-http://192.168.201.162:8088/geoserver/gwc/service/tms/1.0.0/zhjy%3Adongchenggroup@EPSG%3A900913@pbf
+     * //样式文件不好配-不建议使用geoserver矢量切片服务
+     */
+    addGeoServerLayer() {
+      const vt = new VectorTileLayer("geoserver_vt", {
+        urlTemplate: `http://192.168.201.162:8088/geoserver/gwc/service/tms/1.0.0/zhjy%3Adongchenggroup@EPSG%3A900913@pbf/{z}/{x}/{y}.pbf`,
+        tms: true,//这个参数必须设置
+        style: "data/json/mapbox-light/style_mapbox_dongcheng.json",
+      });
+      this.groupLayer.addLayer(vt);
     },
   },
 };
