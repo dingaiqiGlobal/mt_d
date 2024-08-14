007e03a458aea1f3421384351b4ac54dcc2cdf5c
ding <1621029739@qq.com>
2024-08-14 17:20:35 +0800

    添加贴图自发光  泛光

--- src/components/layer/layer_Mapbox_Colourful.vue
+++ src/components/layer/layer_Mapbox_Colourful.vue
@@ -30,6 +30,10 @@ export default {
       vtLayer: null,
       options: [
         {
+          value: "data/json/colourful/style_bloom.json",
+          label: "贴图发光",
+        },
+        {
           value: "data/json/colourful/style_default_blue.json",
           label: "默认蓝",
         },
@@ -70,7 +74,7 @@ export default {
           label: "自然绿",
         },
       ],
-      value: "默认蓝",
+      value: "贴图发光",
     };
   },
 
@@ -94,7 +98,19 @@ export default {
     /**
      * groupLayer
      */
-    const sceneConfig = {};
+    const sceneConfig = {
+      postProcess: {
+        enable: true,
+        antialias: { enable: true },
+        bloom: {
+          //开启泛光
+          enable: true,
+          threshold: 0,
+          factor: 1,
+          radius: 0.02,
+        },
+      },
+    };
     this.groupLayer = new GroupGLLayer("group", [], {
       sceneConfig,
     });
@@ -103,12 +119,20 @@ export default {
   },
 
   methods: {
+    /**
+     * 泛光注意事项
+     * VectorTileLayer---enableBloom: true--不需要
+     * sceneConfig---bloom: {enable: true}--必要
+     * symbol--- "lineBloom": true,--必要
+     */
     addMapBoxLayer() {
       this.vtLayer = new VectorTileLayer("mapbox_vt", {
         urlTemplate: `http://192.168.201.162:8088/geoserver/gwc/service/tms/1.0.0/zhjy%3Adongchenggroup@EPSG%3A900913@pbf/{z}/{x}/{y}.pbf`,
         tms: true, //这个参数必须设置
-        style: "data/json/colourful/style_default_blue.json",
-      }).addTo(this.map);
+        style: "data/json/colourful/style_bloom.json",
+        // enableBloom: true,
+      });
+      this.groupLayer.addLayer(this.vtLayer);
     },
     changeStyle(value) {
       this.vtLayer.setStyle(value);
